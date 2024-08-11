d5304cfe9f9da8102ea588aec865f7ede64fff4e
Orwa Watad <theorwa@gmail.com>
2024-08-11 22:12:56 +0300

    Update

--- src/models/question.ts
+++ src/models/question.ts
@@ -1,8 +1,13 @@
 export interface Question {
     id: number;
     ayahText: string;
+    ayahTextTshkeel: string;
     surahName: string;
+    surahNameTshkeel: string;
     surahNumber: number;
     surahAyahNumber: number;
     pageNumber: number;
+    allAyahNumber: number;
+    juzNumber: number;
+    rubNumber: number;
 }
