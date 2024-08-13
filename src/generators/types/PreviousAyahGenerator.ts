028a75f34f27aff396e537141eb3556c662701b8
Orwa Watad <theorwa@gmail.com>
2024-08-13 04:42:08 +0300

    Update

--- src/generators/types/PreviousAyahGenerator.ts
+++ src/generators/types/PreviousAyahGenerator.ts
@@ -9,20 +9,25 @@ export class PreviousAyahGenerator extends BaseQuestionGenerator {
 
         const randomIndex = Math.floor(Math.random() * Math.max(ayahs.length - 1, 1)) + 5;
         const ayah = ayahs[randomIndex];
-        let previousAyahs = ayahs.slice(Math.max(randomIndex - 5, 0), randomIndex);
+        let previousAyahs = [];
+
+        for (let i = randomIndex - 1; i >= 0 && previousAyahs.length < 5; i--) {
+            const candidateAyah = ayahs[i];
+            if (previousAyahs.length === 0 || !this.isFirst5WordsSame(candidateAyah.ayahText, ayah.ayahText)) {
+                previousAyahs.push(candidateAyah);
+            }
+        }
 
         if (previousAyahs.length < 5) {
             const additionalAyahs = this.getRandomOptions(ayahs, previousAyahs, 5 - previousAyahs.length);
-            previousAyahs = additionalAyahs.concat(previousAyahs);
+            previousAyahs = previousAyahs.concat(additionalAyahs);
         }
 
         if (!ayah || !previousAyahs[previousAyahs.length - 1]) {
             throw new Error('Failed to generate a valid question.');
         }
 
-        // The last ayah in previousAyahs should be the correct one
         const correctAyah = previousAyahs[previousAyahs.length - 1];
-
         const shuffledOptions = this.shuffleArray(previousAyahs);
 
         return {
@@ -32,6 +37,12 @@ export class PreviousAyahGenerator extends BaseQuestionGenerator {
             options: shuffledOptions.map((option) => this.formatAyahText(option.ayahText)),
             correct: shuffledOptions.findIndex(option => option === correctAyah),
         };
+    }
+
+    private isFirst5WordsSame(ayahText1: string, ayahText2: string): boolean {
+        const words1 = ayahText1.split(' ').slice(0, 5).join(' ');
+        const words2 = ayahText2.split(' ').slice(0, 5).join(' ');
+        return words1 === words2;
     }
 
     public get questionText(): string {
