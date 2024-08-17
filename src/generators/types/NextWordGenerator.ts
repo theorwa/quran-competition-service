5a70542e181fc6bcf1384eabc8e2249797de158e
Orwa Watad <theorwa@gmail.com>
2024-08-17 18:06:13 +0300

    Next Word Generator

--- src/generators/types/NextWordGenerator.ts
+++ src/generators/types/NextWordGenerator.ts
@@ -8,19 +8,18 @@ export class NextWordGenerator extends BaseQuestionGenerator {
     protected generateQuestion(filteredAyahs: FilteredAyahs, currentIndex: number): Question {
         const word = filteredAyahs.getWordByIndex(currentIndex);
         const ayah = filteredAyahs.getAyahByWordIndex(currentIndex);
-        const nextWord = filteredAyahs.getWordByIndex(currentIndex + 1);
-        const nextWords = filteredAyahs.getNextUniqueWords(currentIndex, 4);
+        const nextWords = filteredAyahs.getNextUniqueWords(currentIndex, 5);
         if (!nextWords || nextWords.length < 4) {
             throw new Error('Failed to generate a valid question.');
         }
-        nextWords.push(nextWord);
+        const correctOption = nextWords[0];
         const shuffledOptions = this.shuffleArray(nextWords);
         return {
             question: NextWordGenerator.QUESTION_TEXT,
             ayah: word,
             ayahNumber: `${ayah.surahName}:${ayah.surahAyahNumber}`,
             options: shuffledOptions,
-            correct: shuffledOptions.findIndex(option => option === nextWord),
+            correct: shuffledOptions.findIndex(option => option === correctOption),
         };
     }
 
