1f12b26cc9218352e6ea8b2021444ff76bbf1aef
Orwa Watad <theorwa@gmail.com>
2024-08-17 06:06:43 +0300

    Update .gitignore to exclude build files and .iml files

--- dist/specifications/AllAyahsSpecification.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AllAyahsSpecification = void 0;
-class AllAyahsSpecification {
-    isSatisfiedBy(ayah) {
-        return true;
-    }
-}
-exports.AllAyahsSpecification = AllAyahsSpecification;
