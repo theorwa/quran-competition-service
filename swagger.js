043d646e20be4971a949b0d3784173c28079b25a
Orwa Watad <theorwa@gmail.com>
2024-09-08 21:28:24 +0300

    swagger

--- swagger.js
+++ swagger.js
@@ -7,7 +7,7 @@ const doc = {
         description: 'API documentation for the Quran Competition Service',
     },
     host: process.env.HEROKU_APP_URL || 'localhost:3000',
-    schemes: ['https'], // Ensure this matches your Heroku URL scheme
+    schemes: process.env.NODE_ENV === 'production' ? ['https'] : ['http'],
 };
 
 const outputFile = './dist/swagger_output.json';
